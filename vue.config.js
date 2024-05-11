e1620dcab407dd9a04728d731de15fdcaf187096
Pibo Shongwe <piboman@yahoo.com>
2024-05-11 04:41:38 +0200

    update axios call

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
 
 module.exports = defineConfig({
-  configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin({
-        'process.env.VUE_APP_ORDERS_API': JSON.stringify(
-          process.env.ENV === 'prod' || process.env.ENV === 'dev'
-            ? `https://orda-v10-backend-${process.env.ENV}.onrender.com/api/v1/orders`
-            : 'http://localhost:5000/api/v1/orders'
-        )
-      })
-    ]
-  },
   devServer: {
     port: 8080
   },
