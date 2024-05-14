3fd45be0535f4999ae648978cd63b5abb1da9b9d
Pibo Shongwe <piboman@yahoo.com>
2024-05-14 06:18:33 +0200

    update

--- src/components/OrdaRestaurant.vue
+++ src/components/OrdaRestaurant.vue
@@ -1,277 +1,239 @@
 <template>
-  <div>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-        <title>Self Service Order</title>
-    </head>
-<main id="app">
-    <body> 
-            <section class="items">
-                <h4>Pick your items</h4>
-                <div class="product selected">
-                    <div class="photo">
-                        <img src="../assets/img/big-mac.png">
-                    </div>
-                    <div class="description">
-                        <span class="name">Big Mac</span>
-                        <span class="price">$ 5.99</span>
-                        <div class="quantity-area">
-                            <button>-</button>
-                            <span class="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="product">
-                    <div class="photo">
-                        <img src="../assets/img/mc-chicken.png">
-                    </div>
-                    <div class="description">
-                        <span class="name">Mc Chicken</span>
-                        <span class="price">$ 4.99</span>
-                        <div class="quantity-area">
-                            <button>-</button>
-                            <span class="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="product">
-                    <div class="photo">
-                        <img src="../assets/img/double-cb.png">
-                    </div>
-                    <div class="description">
-                        <span class="name">Double Cheese Burger</span>
-                        <span class="price">$ 2.99</span>
-                        <div class="quantity-area">
-                            <button>-</button>
-                            <span class="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="product">
-                    <div class="photo">
-                        <img src="../assets/img/fries.png">
-                    </div>
-                    <div class="description">
-                        <span class="name">Fries</span>
-                        <span class="price">$ 2.99</span>
-                        <div class="quantity-area">
-                            <button>-</button>
-                            <span class="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="product">
-                    <div class="photo">
-                        <img src="../assets/img/nuggets.png">
-                    </div>
-                    <div class="description">
-                        <span class="name">Mc Nuggets</span>
-                        <span class="price">$ 3.49</span>
-                        <div class="quantity-area">
-                            <button>-</button>
-                            <span class="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="product">
-                    <div class="photo">
-                        <img src="../assets/img/salad.png">
-                    </div>
-                    <div class="description">
-                        <span class="name">Salad</span>
-                        <span class="price">$ 2.79</span>
-                        <div class="quantity-area">
-                            <button>-</button>
-                            <span class="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="product">
-                    <div class="photo">
-                        <img src="../assets/img/cola.png">
-                    </div>
-                    <div class="description">
-                        <span class="name">Coke</span>
-                        <span class="price">$ 1.99</span>
-                        <div class="quantity-area">
-                            <button>-</button>
-                            <span class="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="product">
-                    <div class="photo">
-                        <img src="../assets/img/lipton.png">
-                    </div>
-                    <div class="description">
-                        <span class="name">Ice Tea</span>
-                        <span class="price">$ 1.99</span>
-                        <div class="quantity-area">
-                            <button>-</button>
-                            <span class="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-                <div class="product">
-                    <div class="photo">
-                        <img src="../assets/img/water.png">
-                    </div>
-                    <div class="description">
-                        <span class="name">Water</span>
-                        <span class="price">$ 1.49</span>
-                        <div class="quantity-area">
-                            <button>-</button>
-                            <span class="quantity">1</span>
-                            <button>+</button>
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <section class="summary">
-                <strong>Order Details</strong>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Item</th>
-                            <th>Total</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1x Big Mac</td>
-                            <td>5.99</td>
-                        </tr>
-                        
-                        <tr>
-                            <th>Total</th>
-                            <th>5.99</th>
-                        </tr>
-                    </tbody>
-                </table>
-            </section>
-
-
-    </body> 
-</main>
-  </div> 
-</template>
-
-<script>
-export default {
-  name: 'OrdaRestaurant'
-}
-</script>
-
-<style scoped>
-body {
-margin: 0;
-font-family: 'Open Sans', sans-serif;
-}
-
-main > section.items h4 {
-text-align: center;
-margin-top: 0;
-width: 100%;
-}
-main {
-display: flex;
-justify-content: center;
-align-items:flex-start;
-flex-wrap: wrap;
-padding-top: 20px;
-}
-
-main > section.items {
-display: flex;
-flex-wrap: wrap;
-border: 1px solid lightgrey;
-padding: 20px;
-max-width: 500px;
-min-width: 300px;
-justify-content: center;
-
-}
-
-section.items .product {
-border: 1px solid lightgrey;
-margin: 6px;
-flex: 0 0 calc(33.333% - 24px);
-cursor: pointer;
-text-align: center;
-}
-
-section.items .product.selected {
-border: 2px solid rgb(29, 134, 233);
-
-}
-
-section.items .photo img {
-max-width: 90px;
-}
-
-section.items .description {
-display: flex;
-flex-direction: column;
-align-items: center;
-text-align: center;
-font-size: 11px;
-line-height: 15px;
-}
-
-section.items  .description .price {
-font-weight: bold;
-margin: 4px auto;
-}
-
-section.items  .description .quantity-area {
-margin: 8px auto;
-height: 14px;
-}
-
-section.items  .description .quantity-area button {
-width: 16px;
-height: 16px;
-display: inline-flex;
-justify-content: center;
-align-items: center;
-cursor: pointer;
-}
-
-section.items  .description .quantity-area .quantity {
-font-weight: bold;
-margin: 0 4px;
-}
-
-
-section.summary {
-background-color: rgb(245, 245, 245);
-padding: 20px;
-min-height: 200px;
-min-width: 200px;
-text-align: center;
-}
-
-section.summary table {
-width: 100%;
-padding-top: 12px;
-font-size: 11px;
-margin: auto;
-}
-
-section.summary table tbody tr:last-of-type th {
-border-top: 1px solid black;
-padding-top: 4px;
-}
-</style>
+    <div>
+      <main id="app">
+        <section class="items">
+          <h4>Pick your items</h4> 
+          <div v-for="product in products" :key="product.name"
+               class="product"
+               @click="toggleActive(product)"
+               :class="{selected: product.active}">
+            <div class="photo">
+              <img :src="product.photo" :alt="product.name">
+            </div>
+            <div class="description">
+              <span class="name">{{ product.name }}</span>
+              <span class="price">$ {{ product.price }}</span>
+              <div class="quantity-area" v-if="product.active">
+                <button :disabled="product.quantity <= 1" @click.stop="product.quantity--">-</button>
+                <span class="quantity">{{ product.quantity }}</span>
+                <button @click.stop="product.quantity++">+</button>
+              </div>
+            </div>
+          </div>
+        </section>
+  
+        <section class="summary" v-if="total() > 0">
+          <strong>Order Details</strong>
+          <table>
+            <thead>
+              <tr>
+                <th>Item</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="product in activeProducts" :key="'summary-' + product.name">
+                <td>{{ product.quantity + 'x ' + product.name }}</td>
+                <td>${{ (product.quantity * product.price).toFixed(2) }}</td>
+              </tr>
+              <tr>
+                <th>Total</th>
+                <th>${{ total() }}</th>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </main>
+    </div> 
+  </template>
+  
+  <script>
+  export default {
+    name: 'OrdaRestaurant',
+    data() {
+      return {
+        products: [
+          {
+            "photo": "/img/big-mac.png",
+            "name": "Big Mac",
+            "price": 5.99,
+            "active": false,
+            "quantity": 1
+          },
+          {
+            "photo": "/img/mc-chicken.png",
+            "name": "Mc Chicken",
+            "price": 4.99,
+            "active": false,
+            "quantity": 1
+          },
+          {
+            "photo": "/img/double-cb.png",
+            "name": "Double Cheese Burger",
+            "price": 2.99,
+            "active": false,
+            "quantity": 1
+          },
+          {
+            "photo": "/img/fries.png",
+            "name": "Fries",
+            "price": 2.99,
+            "active": false,
+            "quantity": 1
+          },
+          {
+            "photo": "/img/nuggets.png",
+            "name": "Mc Nuggets",
+            "price": 3.49,
+            "active": false,
+            "quantity": 1
+          },
+          {
+            "photo": "/img/salad.png",
+            "name": "Salad",
+            "price": 2.79,
+            "active": false,
+            "quantity": 1
+          },
+          {
+            "photo": "/img/cola.png",
+            "name": "Coke",
+            "price": 1.99,
+            "active": false,
+            "quantity": 1
+          },
+          {
+            "photo": "/img/lipton.png",
+            "name": "Ice Tea",
+            "price": 1.99,
+            "active": false,
+            "quantity": 1
+          },
+          {
+            "photo": "/img/water.png",
+            "name": "Water",
+            "price": 1.49,
+            "active": false,
+            "quantity": 1
+          }
+        ]
+      }
+    },
+    computed: {
+      activeProducts() {
+        return this.products.filter(p => p.active);
+      }
+    },
+    methods: {
+      toggleActive(product) {
+        product.active = !product.active;
+      },
+      total() {
+        return this.activeProducts.reduce((acc, product) => {
+          return acc + product.price * product.quantity;
+        }, 0).toFixed(2);
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  body {
+    margin: 0;
+    font-family: 'Open Sans', sans-serif;
+  }
+  
+  main > section.items h4 {
+    text-align: center;
+    margin-top: 0;
+    width: 100%;
+  }
+  
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    flex-wrap: wrap;
+    padding-top: 20px;
+  }
+  
+  main > section.items {
+    display: flex;
+    flex-wrap: wrap;
+    border: 1px solid lightgrey;
+    padding: 20px;
+    max-width: 500px;
+    min-width: 300px;
+    justify-content: center;
+  }
+  
+  section.items .product {
+    border: 1px solid lightgrey;
+    margin: 6px;
+    flex: 0 0 calc(33.333% - 24px);
+    cursor: pointer;
+    text-align: center;
+  }
+  
+  section.items .product.selected {
+    border: 2px solid rgb(29, 134, 233);
+  }
+  
+  section.items .photo img {
+    max-width: 90px;
+  }
+  
+  section.items .description {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    font-size: 11px;
+    line-height: 15px;
+  }
+  
+  section.items .description .price {
+    font-weight: bold;
+    margin: 4px auto;
+  }
+  
+  section.items .description .quantity-area {
+    margin: 8px auto;
+    height: 14px;
+  }
+  
+  section.items .description .quantity-area button {
+    width: 16px;
+    height: 16px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+  }
+  
+  section.items .description .quantity-area .quantity {
+    font-weight: bold;
+    margin: 0 4px;
+  }
+  
+  section.summary {
+    background-color: rgb(245, 245, 245);
+    padding: 20px;
+    min-height: 200px;
+    min-width: 200px;
+    text-align: center;
+  }
+  
+  section.summary table {
+    width: 100%;
+    padding-top: 12px;
+    font-size: 11px;
+    margin: auto;
+  }
+  
+  section.summary table tbody tr:last-of-type th {
+    border-top: 1px solid black;
+    padding-top: 4px;
+  }
+  </style>
+  
--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-const app = createApp(App).use(router);
+const app = createApp(App);
 app.use(router);
 app.mount('#app');
